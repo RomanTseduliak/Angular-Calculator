5ba74691c61d137038a36270457b2c14b6d0db22
RomanTseduliak <alligarch@gmail.com>
2021-01-27 14:22:36 +0200

    uiDone

--- src/app/user-interface/user-interface.component.ts
+++ src/app/user-interface/user-interface.component.ts
@@ -1,15 +1,91 @@
+import { Buttons } from './user-interface.constants';
+import { ValueConverter } from '@angular/compiler/src/render3/view/template';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user-interface',
   templateUrl: './user-interface.component.html',
-  styleUrls: ['./user-interface.component.scss']
+  styleUrls: ['./user-interface.component.scss'],
 })
 export class UserInterfaceComponent implements OnInit {
+  constructor() {}
+  buttons = Buttons;
+  subText = '';
+  mainText = '';
+  operand1: number;
+  operand2: number;
+  operator = 'null';
+  calculationString = '';
+  answered = false;
+  operatorSet = false;
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  haveValue(key: Object) {
+    if (key === '/' || key === 'x' || key === '-' || key === '+') {
+      const lastKey = this.mainText[this.mainText.length - 1];
+      if (
+        lastKey === '/' ||
+        lastKey === 'x' ||
+        lastKey === '-' ||
+        lastKey === '+'
+      ) {
+        this.operatorSet = true;
+      }
+      if (this.operatorSet || this.mainText === '') {
+        return;
+      }
+      this.operand1 = parseFloat(this.mainText);
+      this.operator = this.operator;
+      this.operatorSet = true;
+    }
+    if (this.mainText.length === 10) {
+      return;
+    }
+    this.mainText += key;
   }
 
+  getAnswer() {
+    this.calculationString = this.mainText;
+    this.operand2 = parseFloat(this.mainText.split(this.operator)[1]);
+    if (this.operator === '/') {
+      this.subText = this.mainText;
+      this.mainText = (this.operand1 / this.operand2).toString();
+      this.subText = this.calculationString;
+      if (this.mainText.length > 9) {
+        this.mainText = this.mainText.substr(0, 9);
+      }
+    } else if (this.operator === 'x') {
+      this.subText = this.mainText;
+      this.mainText = (this.operand1 * this.operand2).toString();
+      this.subText = this.calculationString;
+      if (this.mainText.length > 9) {
+        this.mainText = 'ERROR';
+        this.subText = 'Range Exceeded';
+      }
+    } else if (this.operator === '-') {
+      this.subText = this.mainText;
+      this.mainText = (this.operand1 - this.operand2).toString();
+      this.subText = this.calculationString;
+    } else if (this.operator === '+') {
+      this.subText = this.mainText;
+      this.mainText = (this.operand1 + this.operand2).toString();
+      this.subText = this.calculationString;
+      if (this.mainText.length > 9) {
+        this.mainText = 'ERROR';
+        this.subText = 'Range Exceeded';
+      }
+    } else {
+      this.subText = 'ERROR: Invalid Operation';
+    }
+    this.answered = true;
+  }
+  clear() {
+    if (this.calculationString != '') {
+      this.calculationString = this.calculationString.substr(
+        0,
+        this.calculationString.length - 1
+      );
+    }
+  }
 }
---
+++ src/app/user-interface/user-interface.constants.ts
@@ -0,0 +1,83 @@
+export const Buttons = [
+  {
+    name: 'C',
+    type: 'opers',
+  },
+  {
+    name: '+/-',
+    type: 'opers',
+  },
+  {
+    name: '%',
+    type: 'opers',
+  },
+  {
+    name: '/',
+    type: 'opers',
+    background: '#64cbf7',
+  },
+  {
+    name: '7',
+    type: 'digit',
+  },
+  {
+    name: '8',
+    type: 'digit',
+  },
+  {
+    name: '9',
+    type: 'digit',
+  },
+  {
+    name: 'x',
+    type: 'opers',
+    background: '#64cbf7',
+  },
+  {
+    name: '4',
+    type: 'digit',
+  },
+  {
+    name: '5',
+    type: 'digit',
+  },
+  {
+    name: '6',
+    type: 'digit',
+  },
+  {
+    name: '-',
+    type: 'opers',
+    background: '#64cbf7',
+  },
+  {
+    name: '1',
+    type: 'digit',
+  },
+  {
+    name: '2',
+    type: 'digit',
+  },
+  {
+    name: '3',
+    type: 'digit',
+  },
+  {
+    name: '+',
+    type: 'opers',
+    background: '#64cbf7',
+  },
+  {
+    name: '0',
+    type: 'digit',
+  },
+  {
+    name: '.',
+    type: 'opers',
+  },
+  {
+    name: '=',
+    type: 'opers',
+    background: '#64cbf7',
+  },
+];
